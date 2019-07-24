ebc95577f9b207d0b3e35952c72835eb15b09076
naufal37 <naufalahmadfauz@gmail.com>
2019-07-24 19:59:36 +0700

    berhasil bab model

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'latihan'],function(){
+  Route::get('/kategori/all','CategoryController@index');
+  Route::get('/kategori/search','CategoryController@search');
+  Route::get('/kategori/{id}/delete','CategoryController@delete');
+  Route::get('/kategori/{id}/restore','CategoryController@restore');
+  Route::get('/kategori/{id}/permanent-delete','CategoryController@permanentdelete');
+});
